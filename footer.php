884422130f9751e3b17ded685ef104a8a6d1682d
Cyberister <cts.rail@gmail.com>
2025-03-31 08:36:05 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="d-flex flex-column flex-md-row justify-content-center align-items-center">
             <p class="mx-3 mb-2 mb-md-0">Contact:</p>
             <a href="mailto:593044@edu.rocmn.nl" class="mx-3 mb-2 mb-md-0">593044@edu.rocmn.nl</a>
-            <a href="tel:+31612548932" class="mx-3 mb-2 mb-md-0">0612548932</a>  
+            <a href="tel:+31612548932" class="mx-3 mb-2 mb-md-0">...</a>  
             <p class="mx-3 mb-2 mb-md-0">Ⓐ created by Heer Kocak</p>
         </div>
     </div>
